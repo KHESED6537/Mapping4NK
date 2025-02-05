38f2463e5dec37d63d329c69bc74cc46862fc291
KHESED6537 <mindstate82@gmail.com>
2025-02-05 18:28:04 +0900

    Spring Boot project initialized

---
+++ src/test/java/com/mercy/data_analysis/DataAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercy.data_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
